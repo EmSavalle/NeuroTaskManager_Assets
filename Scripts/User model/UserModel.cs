055a707e8c0ca28eadeaf979ef619eeffc17938c
esavalle <emile.savalle@inria.fr>
2024-11-18 16:27:28 +0100

    Pilot

--- Scripts/User model/UserModel.cs
+++ Scripts/User model/UserModel.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using System.Text.Json;
 using System.IO;
 
 public class UserModel : MonoBehaviour
